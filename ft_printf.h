ee68fe63e33f2c96e25bba977b0884b3f12c0363
AjinChris95 <87316448+AjinChris95@users.noreply.github.com>
2023-11-20 10:28:45 +0100

    Update ft_printf.h

---
+++ ft_printf.h
@@ -0,0 +1,19 @@
+#ifndef FT_PRINTF_H
+# define FT_PRINTF_H
+# include <string.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include <stdio.h>
+
+size_t	ft_strlen(const char *str);
+size_t	ft_putchar(int c);
+size_t	ft_putstr(char *s);
+void	ft_putnbr(int d, int *count);
+void	ft_putnbr_u(unsigned int u, int *count);
+int		ft_putnbr_base_x(unsigned int nbr);
+int		ft_putnbr_base(unsigned int nbr);
+int		ft_putnbr_base_p(unsigned long int nbr);
+int		ft_printf(const char *str, ...);
+int		count_nbr(const char *s, int *l, int *i);
+void	if_s(va_list args, int *count, int count2);
+#endif
